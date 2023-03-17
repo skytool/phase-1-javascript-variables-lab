3e822782e6699b959d6d60611c9d3ae995bce182
skytool <albert.do@aol.com>
2023-03-16 21:01:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+function companyName(){
+    const companyName = companyName;
+}
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
